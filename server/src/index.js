75646d9c94fc36bc8914504417b67fc5721a31bc
hendo12 <henrydoce@protonmail.com>
2023-05-11 09:30:18 -0500

    fixed import

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,5 @@
 const { ApolloServer } = require("@apollo/server");
-const { startStandaloneServer } = require("@apollo/server/standalone"
+const { startStandaloneServer } = require("@apollo/server/standalone");
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 const TrackAPI = require("./datasources/track-api");
